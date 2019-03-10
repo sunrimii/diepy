73a1f71916ed99e5131d95105ae7058c17784192
Roger <sunrimii@gmail.com>
2019-03-10 15:26:41 +0800

    test

--- config.py
+++ config.py
@@ -1,47 +1,10 @@
-BG_COLOR = (210, 210, 210)
-HP_COLOR = (100, 200, 100)
+COLOR_OF_BG = (210, 210, 210)
+COLOR_OF_GRID = (200, 200, 200)
+COLOR_OF_HP = (100, 200, 100)
 BLACK = (0, 0, 0)
-DARK_BLUE = (20, 143, 175)
 BLUE = (20, 180, 223)
+DARK_BLUE = (20, 143, 175)
 DARK_GRAY = (123, 123, 123)
-GRAY = (157, 157, 157)
 YELLOW = (255, 232, 106)
 DARK_YELLOW = (190, 176, 78)
-
-TANK_MAX_HP = 10
-TANK_HP_REGEN = 0.5
-TANK_DAMAGE = 3
-TANK_ACC = 0.02
-TANK_RELOADING_TIME = 500
-TANK_INIT_POS = (500, 500)
-TANK_MAX_SPEED = 1.7
-TANK_RECOIL = 0.01
-
-"""
-子彈 若有重複則按比率
-    橢圓形 爆炸
-    三角形 穿透
-    正方形 緩速
-    十字形 治療
-    隕石型 燃燒
-
-砲管
-    增加長度 射程
-    加粗寬度 大小
-    兩段砲管 後座
-    閃電標誌 冷卻
-    增加個數 
-
-本體
-    
-
-
-
-子彈增加血量 加粗外框 
-子彈增加治療 子彈畫十字
-子彈能緩速 外觀變方形
-
-
-
-pygame.gfxdraw.filled_circle(self.image, 15, 15, 12, BLUE)
-"""
+GRAY = (157, 157, 157)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randrange
 
 import pygame
 import pygame.gfxdraw
-from pygame.locals import *
+import pygame.locals
 
 from config import *
 
@@ -12,7 +12,6 @@ class ExpSquare(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.exp = 5
         self.hp = 3
         self.max_hp = 3
         self.damage = 5
@@ -23,8 +22,8 @@ class ExpSquare(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(x, y)
 
         self.image_orig = pygame.Surface((35, 35)).convert()
-        pygame.gfxdraw.box(self.image_orig, (0, 0, 35, 35), DARK_YELLOW)
-        pygame.gfxdraw.box(self.image_orig, (3, 3, 29, 29), YELLOW)
+        pygame.gfxdraw.box(self.image_orig, (0, 0, 35, 35), DARK_GRAY)
+        pygame.gfxdraw.box(self.image_orig, (3, 3, 29, 29), GRAY)
         self.image_orig.set_colorkey(BLACK)
         self.image_alpha = 254
         self.image_scale = (self.image_orig.get_width(), self.image_orig.get_height())
@@ -49,10 +48,11 @@ class ExpSquare(pygame.sprite.Sprite):
             # 完全淡出後移除
             if self.image_alpha < 0:
                 self.kill()
+        
         # 顯示血量
         elif self.hp < self.max_hp:
             # 新建加長的畫布
-            new_image = pygame.Surface((self.image.get_width(), self.image.get_height()+15), SRCALPHA)
+            new_image = pygame.Surface((self.image.get_width(), self.image.get_height()+15), pygame.locals.SRCALPHA)
             # 在上方先畫上原本的
             new_image.blit(self.image, (0,0))
             # 再畫上血條
@@ -64,7 +64,7 @@ class ExpSquare(pygame.sprite.Sprite):
             w *= self.hp/self.max_hp
             w -=boarder*2
             h -= boarder*2
-            pygame.gfxdraw.box(new_image, (x, y, w, h), HP_COLOR)
+            pygame.gfxdraw.box(new_image, (x, y, w, h), COLOR_OF_HP)
             
             self.image = new_image
             self.rect.height +=7
@@ -73,144 +73,98 @@ class ExpSquare(pygame.sprite.Sprite):
         self._check_moving()
         self._check_hp()
 
-class SmallBullet(pygame.sprite.Sprite):
-    def __init__(self, bullet_max_hp, bullet_damage, bullet_speed, tank_pos, tank_degree, barrel_width):
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, color_of_body, color_of_border, pos, degree):
         super().__init__()
-
-        self.birth_time = pygame.time.get_ticks()
         
-        self.max_hp = bullet_max_hp
+        self.time_of_birth = pygame.time.get_ticks()
+        
+        self.max_hp = 1
         self.hp = self.max_hp
-        self.damage = bullet_damage
-        self.speed = pygame.math.Vector2(bullet_speed, 0).rotate(-tank_degree)
 
-        # 偏移的方向與彈藥方向相同 長度為砲管的長度
-        pos_offset = pygame.math.Vector2(self.speed)
-        pos_offset.scale_to_length(barrel_width)
-        # 起始位置為砲口
-        self.pos = tank_pos + pos_offset
+        self.speed = pygame.math.Vector2(3, 0).rotate(-degree)
+        # 將起始位置從坦克中心移至砲口
+        offset = pygame.math.Vector2(self.speed)
+        offset.scale_to_length(50)
+        self.pos = pos + offset
 
-        self.image = pygame.Surface((30, 30)).convert()
+        self.image = pygame.Surface((30, 30))
+        x = self.image.get_width()//2
+        y = self.image.get_height()//2
+        r = x
+        pygame.gfxdraw.filled_circle(self.image, x, y, r, color_of_border)
+        border = 3
+        r -= border
+        pygame.gfxdraw.filled_circle(self.image, x, y, r, color_of_body)
+
         self.image.set_colorkey(BLACK)
-        pygame.draw.circle(self.image, BLUE, (15, 15), 12, 0)
-        pygame.draw.circle(self.image, DARK_BLUE, (15, 15), 12, 3)
-        self.rect = self.image.get_rect(center=self.pos)
+        self.image.set_alpha(0)
         self.mask = pygame.mask.from_surface(self.image)
-        self.image_alpha = 254
+        self.rect = self.image.get_rect(center=self.pos)
+        
+        # 紀錄剛碰撞的敵人 避免短時間內不斷檢測
+        self.whitelist = pygame.sprite.Group()
 
-        # 紀錄剛碰撞的敵人 避免重複檢測
-        self.just_collide_enemy = pygame.sprite.Group()
-
-    def _check_moving(self):
+    def update(self, *arg):
         self.pos += self.speed
         self.rect.center = self.pos
-
-    def _check_hp(self):
-        # 限制持續時間
-        alive_time = pygame.time.get_ticks() - self.birth_time
-        if self.hp <= 0 or alive_time > 500:
+        
+        # 外觀動畫
+        alive_time = pygame.time.get_ticks() - self.time_of_birth
+        if alive_time<1500 and self.hp>0 and self.image.get_alpha()!=255:
+            # 出生時淡入
+            alpha = self.image.get_alpha() + 15
+            self.image.set_alpha(alpha)
+        elif alive_time>1500 or self.hp<=0:
             # 消失時放大
             scale = int(self.image.get_width()*1.06), int(self.image.get_height()*1.06)
             self.image = pygame.transform.scale(self.image, scale)
             self.rect = self.image.get_rect(center=self.pos)
             # 消失時淡出
-            self.image.set_alpha(self.image_alpha)
-            self.image_alpha -= 20
+            alpha = self.image.get_alpha() - 20
+            self.image.set_alpha(alpha)
             # 完全淡出後移除
-            if self.image_alpha < 0:
+            if self.image.get_alpha() is 0:
                 self.kill()
-    
-    # def _check_collide(self):
-    #     # 所有命中的敵人
-    #     for enemy in pygame.sprite.spritecollide(self, enemy_group, False, pygame.sprite.collide_mask):
-    #         can_attack = False if self.just_collide_enemy.has(enemy) else True
-    #         if self.hp>0 and enemy.hp>0 and can_attack:
-    #             # 互相傷害
-    #             self.hp -= enemy.damage
-    #             enemy.hp -= self.damage
-    #             self.just_collide_enemy.add(enemy)
-    #             # 若殺死則得到經驗值
-    #             if enemy.hp < 0:
-    #                 # self.player_exp += enemy.exp
-    
-    def update(self, *arg):
-        self._check_moving()
-        self._check_hp()
-        # self._check_collide()
 
 class Tank(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, color_of_body, color_of_border):
         super().__init__()
 
-        self.max_hp = 10
-        self.hp_regen = 0.1
-        self.damage = 1
+        self.color_of_body = color_of_body
+        self.color_of_border = color_of_border
+        
         self.acc = 0.03
+        self.pos = pygame.math.Vector2(500, 500)
+        self.speed = pygame.math.Vector2(0, 0)
+        self.max_speed = 1.7
+        self.recoil = pygame.math.Vector2(0.000, 0)
+        
+        self.scale_of_barrel = 1
+        
+        self.can_attack = True
+        self.time_of_attacking = 0
         self.reloading_time = 500
 
-        self.pos = pygame.math.Vector2(TANK_INIT_POS)
-        self.speed = pygame.math.Vector2(0, 0)
-        self.max_speed = TANK_MAX_SPEED
-        self.recoil = pygame.math.Vector2(TANK_RECOIL, 0)
+        # 鏡頭移動用
+        self.dx = 0
+        self.dy = 0
 
-        self.bullet_type = SmallBullet
-        self.barrel_scale = 1
-        self.barrel_width = 10
-        
-        # 用於判斷是否可再攻擊
-        self.can_attack = True
-        self.attack_time = 0
+        # 紀錄剛碰撞的敵人 避免短時間內不斷檢測
+        self.whitelist = pygame.sprite.Group()
 
-        self.image = pygame.Surface((100, 100)).convert()
-        self.rect = self.image.get_rect()
-        self.image.set_colorkey(BLACK)
-
-    def _update_image(self, mouse_pos):
-        # 旋轉角度(https://stackoverflow.com/questions/10473930/how-do-i-find-the-angle-between-2-points-in-pygame)
-        # 注意遊戲的座標與常用座標不同(Y軸相反)使用時須加上負號
-        mouse_x, mouse_y = mouse_pos
-        dx, dy = mouse_x-self.pos.x, mouse_y-self.pos.y
-        self.degree = degrees(atan2(-dy, dx) % (2*pi))
-        
-        # 清除畫布
-        self.image.fill(BLACK)
-        
-        # 砲管頂點
-        # p1(x1,y1)---p4(x2,y1)
-        # p2(x1,y2)---p3(x2,y2)
-        x1, y1 = 50, 40
-        x2, y2 = x1+47*self.barrel_scale, 60
-        points = (x1, y1), (x1, y2), (x2, y2), (x2, y1)
-        # 旋轉砲管
-        points = [(point-(50, 50)).rotate(-self.degree)+(50, 50)
-                  for point in map(pygame.math.Vector2, points)]
-        pygame.draw.polygon(self.image, GRAY, points, 0)
-        pygame.draw.polygon(self.image, DARK_GRAY, points, 3)
-        # 本體
-        pygame.draw.circle(self.image, BLUE, (50, 50), 25, 0)
-        pygame.draw.circle(self.image, DARK_BLUE, (50, 50), 25, 3)
-
-        self.mask = pygame.mask.from_surface(self.image)
-
-    def update(self, pressed_keys, mouse_pos, tank_max_hp, tank_hp_regen, tank_damage, tank_acc, reloading_time):
-        # 更新能力值
-        self.max_hp = 10 + tank_max_hp
-        self.hp_regen = 0.1 + tank_hp_regen
-        self.damage = 1 + tank_damage
-        self.acc = 0.03 + tank_acc
-        self.reloading_time = 500 + reloading_time
-
+    def update(self, pressed_keys, is_click, mouse_x, mouse_y):
         # 檢查是否有加速度
-        if pressed_keys[K_w]:
+        if pressed_keys[pygame.locals.K_w]:
             self.speed += (0, -self.acc)
-        elif pressed_keys[K_s]:
+        elif pressed_keys[pygame.locals.K_s]:
             self.speed += (0, self.acc)
         else:
             self.speed.y /= 1.01
             self.speed.y = 0 if abs(self.speed.y) < 0.0000005 else self.speed.y
-        if pressed_keys[K_a]:
+        if pressed_keys[pygame.locals.K_a]:
             self.speed += (-self.acc, 0)
-        elif pressed_keys[K_d]:
+        elif pressed_keys[pygame.locals.K_d]:
             self.speed += (self.acc, 0)
         else:
             self.speed.x /= 1.01
@@ -219,102 +173,156 @@ class Tank(pygame.sprite.Sprite):
         # 限制速度在指定區間內
         self.speed.x = min(self.max_speed, max(-self.max_speed, self.speed.x))
         self.speed.y = min(self.max_speed, max(-self.max_speed, self.speed.y))
-
-        # 更新位置
+        
         self.pos += self.speed
-        self.rect.center = self.pos
 
-        # 冷卻期間伸縮砲管
+        # 旋轉角度(https://stackoverflow.com/questions/10473930/how-do-i-find-the-angle-between-2-points-in-pygame)
+        # 注意遊戲的座標與常用座標不同(Y軸相反)使用時須加上負號
+        dx, dy = mouse_x-self.pos.x, mouse_y-self.pos.y
+        degree = degrees(atan2(-dy, dx) % (2*pi))
+        
+        # 按左鍵發射子彈
+        if self.can_attack and is_click:
+            self.can_attack = False
+            self.time_of_attacking = pygame.time.get_ticks()
+            bullet = Bullet(self.color_of_body, self.color_of_border, self.pos, degree)
+            player.add(bullet)
+
+        # 若發射後的冷卻期間伸縮砲管
         if not self.can_attack:
-            # 從攻擊後所經過的時間
-            elapsed_time = pygame.time.get_ticks() - self.attack_time
-            # 前25%的時間收縮
-            if elapsed_time < self.reloading_time*0.25:
-                self.barrel_scale -= 0.003
-            # 再25%的時間拉長
-            elif elapsed_time < self.reloading_time*0.5:
-                self.barrel_scale += 0.003
+            # 從發射後所經過的時間
+            elapsed_time = pygame.time.get_ticks() - self.time_of_attacking
+            # 前20%的時間收縮
+            if elapsed_time < self.reloading_time*0.2:
+                self.scale_of_barrel -= 0.003
+            # 再20%的時間拉長
+            elif elapsed_time < self.reloading_time*0.4:
+                self.scale_of_barrel += 0.003
                 # 後座力是砲口的反方向
-                self.speed += self.recoil.rotate(180-self.degree)
+                self.speed += self.recoil.rotate(180-degree)
             # 其餘時間將長度恢復原狀
             elif elapsed_time < self.reloading_time:
-                self.barrel_scale = 1
+                self.scale_of_barrel = 1
             # 冷卻結束
             else:
                 self.can_attack = True
 
-        # 由於砲管會收縮因此每次重畫收縮的情況
-        self._update_image(mouse_pos)
+        # 因為砲管會收縮所以每次更新都新建重畫
+        # 砲管以水平開口朝右繪製 後再根據滑鼠旋轉
+        # x,y--------w----------
+        # |                    |
+        # h                    h
+        # |                    |
+        # -----------w----------
+        self.image = pygame.Surface((100, 100))
+        border = 3
 
-class player:
-    def __init__(self):
-        # 玩家的能力值
-        self.tank_max_hp = 0
-        self.tank_hp_regen = 0
-        self.tank_damage = 0
-        self.tank_acc = 0
-        self.reloading_time = 0
-        self.bullet_max_hp = 5
-        self.bullet_damage = 1
-        self.bullet_speed = 4
+        # 畫砲管
+        x = self.image.get_width() // 2
+        y = self.image.get_height() // 5 * 2
+        w = self.image.get_width() // 2 * self.scale_of_barrel
+        h = self.image.get_height() // 5
+        pygame.gfxdraw.box(self.image, (x,y,w,h), DARK_GRAY)
+        x += border
+        y += border
+        w -= border*2
+        h -= border*2
+        pygame.gfxdraw.box(self.image, (x,y,w,h), GRAY)
+        
+        # 畫本體
+        x = self.image.get_width() // 2
+        y = self.image.get_height() // 2
+        r = self.image.get_width() // 4
+        pygame.gfxdraw.filled_circle(self.image, x, y, r, self.color_of_border)
+        r -= border
+        pygame.gfxdraw.filled_circle(self.image, x, y, r, self.color_of_body)
+        
+        self.image = pygame.transform.rotate(self.image, degree)
+        self.image.set_colorkey(BLACK)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(center=self.pos)
 
-        self.player_exp = 0
+    
+        # # 碰撞檢測
+        # for bullet in self.group:
+        #     for enemy in pygame.sprite.spritecollide(bullet, enemies, False, pygame.sprite.collide_mask):
+        #         can_attack = False if bullet.whitelist.has(enemy) else True
+        #         if bullet.hp>0 and enemy.hp>0 and can_attack:
+        #             # 互相傷害
+        #             bullet.hp -= enemy.damage
+        #             enemy.hp -= bullet.damage
+        #             bullet.whitelist.add(enemy)
 
-        self.tank = Tank()
-        self.group = pygame.sprite.Group()
-        self.group.add(self.tank)
+if __name__ == "__main__":
+    pygame.init()
+    icon = pygame.image.load("icon.png")
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption("Diepy")
+    screen = pygame.display.set_mode((0,0))
+    
+    # 建立地圖
+    world = pygame.Surface((5000, 5000))
+    # 填充背景
+    world.fill(COLOR_OF_BG)
+    # 繪製網格
+    world_w, world_h = world.get_size()
+    for x in range(0, world_w, 15):
+        pygame.gfxdraw.box(world, (x,0,1,world_h), COLOR_OF_GRID)
+    for y in range(0, world_h, 15):
+        pygame.gfxdraw.box(world, (0,y,world_w,1), COLOR_OF_GRID)
+    
+    bg = world.copy()
 
-    def update(self):
-        # 按左鍵攻擊時將子彈加入群組
-        is_click = pygame.mouse.get_pressed()[0]
-        if is_click and self.tank.can_attack:
-            self.tank.can_attack = False
-            self.tank.attack_time = pygame.time.get_ticks()
-            bullet = self.tank.bullet_type(self.bullet_max_hp, self.bullet_damage, self.bullet_speed, self.tank.pos, self.tank.degree, self.tank.barrel_width)
-            self.group.add(bullet)
+    clock = pygame.time.Clock()
 
+    enemies = pygame.sprite.Group()
+    player = pygame.sprite.Group()
+
+    tank = Tank(BLUE, DARK_BLUE)
+    player.add(tank)
+
+    is_running = True
+    while is_running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                is_running = False
+        
+        # 生成補充的彈藥
+        # if len(enemies) < 25:
+        #     es = ExpSquare()
+        #     enemies.add(es)
+        
+        # 更新精靈
         pressed_keys = pygame.key.get_pressed()
-        mouse_pos = pygame.mouse.get_pos()
-        self.group.update(pressed_keys, mouse_pos, self.tank_max_hp, self.tank_hp_regen, self.tank_damage, self.tank_acc, self.reloading_time)
+        is_click = pygame.mouse.get_pressed()[0]
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        try:
+            # 加上鏡頭位置的偏移
+            mouse_x += cam.x
+            mouse_y += cam.y
+        except:
+            # 第一次循環沒有鏡頭偏移
+            pass
+        player.update(pressed_keys, is_click, mouse_x, mouse_y)
+        enemies.update()
 
-    def draw(self, screen):
-        self.group.draw(screen)
+        # 更新畫面   
+        enemies.clear(world, bg)
+        player.clear(world, bg)
+        enemies.draw(world)
+        player.draw(world)
+        
+        cam = screen.get_rect()
+        cam.center = tank.pos
+        cam.x = min(max(cam.x,0),1920) * 0.8
+        cam.y = min(max(cam.y,0),1080) * 0.8
+        screen.blit(world.subsurface(cam), (0,0))
 
-pygame.init()
-icon = pygame.image.load("icon.png")
-pygame.display.set_icon(icon)
-pygame.display.set_caption("Diepy")
-screen = pygame.display.set_mode((1920, 1080))
+        pygame.display.update()
 
-# 建立時鐘限制幀率
-clock = pygame.time.Clock()
-# 玩家列表 順序代表第幾位
-players = [player()]
-# 敵人列表 包含經驗塊
-enemy_group = pygame.sprite.Group()
+        # 設定每秒144幀
+        clock.tick(144)
 
-is_running = True
-while is_running:
-    # 關閉遊戲
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            is_running = False
-    # 清除畫面
-    screen.fill(BG_COLOR)
-    # 生成經驗磚
-    if len(enemy_group) < 50:
-        enemy_group.add(ExpSquare())
-
-    enemy_group.update()
-    enemy_group.draw(screen)
-    for player in players:
-        player.update()
-        player.draw(screen)
-
-    pygame.display.update()
-
-    # 設定每秒144幀
-    clock.tick(144)
-
-pygame.quit()
-# sys.exit()
+    pygame.quit()
+    # sys.exit()
